4f2dcf9816dbe1a271cab4e3aed8ca4728bedd44
KaviyaShree-V <karthikavi1976@gmail.com>
2025-05-26 11:40:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (60.02%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,20 +1,42 @@
 class Solution {
     public void rotate(int[][] matrix) 
     {
-        int size = matrix.length;
-        int[][] n = new int[size][size];
-        for(int i=0; i<size; i++)
+        // int size = matrix.length;
+        // int[][] n = new int[size][size];
+        // for(int i=0; i<size; i++)
+        // {
+        //     for(int j=0; j<size; j++)
+        //     {
+        //         n[i][j] = matrix[i][j];
+        //     }
+        // }
+        // for(int i=0; i<size; i++)
+        // {
+        //     for(int j=0; j<size; j++)
+        //     {
+        //         matrix[j][size-1-i] = n[i][j];
+        //     }
+        // }
+
+        for(int i=0; i<matrix.length; i++)
         {
-            for(int j=0; j<size; j++)
+            for(int j=i; j<matrix[0].length; j++)
             {
-                n[i][j] = matrix[i][j];
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
-        for(int i=0; i<size; i++)
+
+        for(int i=0; i<matrix.length; i++)
         {
-            for(int j=0; j<size; j++)
+            for(int j=0; j<(matrix.length)/2; j++)
             {
-                matrix[j][size-1-i] = n[i][j];
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
             }
         }
     }
