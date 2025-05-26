2c1925104e3b2f9d6e52a28bc6a995fa733580ca
KaviyaShree-V <karthikavi1976@gmail.com>
2025-05-26 21:54:45 +0530

    Time: 33 ms (25.50%) | Memory: 45.4 MB (47.79%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -5,14 +5,15 @@ class Solution {
         for(int i=0; i<s.length(); i++)
         {
             char c = s.charAt(i);
-            if(check.containsKey(c))
-            {
-                check.put(c,check.get(c)+1);
-            }
-            else
-            {
-                check.put(c,1);
-            }
+            // if(check.containsKey(c))
+            // {
+            //     check.put(c,check.get(c)+1);
+            // }
+            // else
+            // {
+            //     check.put(c,1);
+            // }
+            check.put(c, check.getOrDefault(c, 0) + 1);
         }
         for(int i=0; i<s.length(); i++)
         {
