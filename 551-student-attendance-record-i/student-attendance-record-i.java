05502ef0d6fc1ae1a47b611720f73ed2c3b08f7f
KaviyaShree-V <karthikavi1976@gmail.com>
2025-06-13 10:46:07 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (31.40%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean checkRecord(String s) 
+    {
+        int countA = 0;
+        int countL = 0;
+
+        for (int i = 0; i < s.length(); i++) 
+        {
+            char ch = s.charAt(i);
+
+            if (ch == 'A') 
+            {
+                countA++;
+                if (countA >= 2) 
+                return false;
+            }
+
+            if (ch == 'L') 
+            {
+                countL++;
+                if (countL >= 3) 
+                return false;
+            } 
+            else 
+            {
+                countL = 0; 
+            }
+        }
+        return true;
+    }
+}
